cfacbff92513a29f01d53bd4cd07ed8150562213
hangillee <skfcb10@naver.com>
2023-02-06 21:50:17 +0900

    init: initial commit for backend repository of Liferary

---
+++ src/main/java/gdsc/skhu/liferary/LiferaryApplication.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.liferary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiferaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiferaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/skhu/liferary/LiferaryApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.liferary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiferaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
