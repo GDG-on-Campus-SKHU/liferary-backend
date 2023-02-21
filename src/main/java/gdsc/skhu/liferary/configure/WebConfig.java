f03dbbbfacfc93475b7854a68893b1c0930ec379
hangillee <skfcb10@naver.com>
2023-02-21 10:36:48 +0900

    chore: edit CORS config

--- src/main/java/gdsc/skhu/liferary/configure/WebConfig.java
+++ src/main/java/gdsc/skhu/liferary/configure/WebConfig.java
@@ -6,14 +6,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "DELETE", "PATCH", "OPTIONS", "HEAD");
     }
 }
