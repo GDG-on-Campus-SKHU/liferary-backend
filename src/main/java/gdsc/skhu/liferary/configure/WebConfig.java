17842593c9b37b5c665dcd7860feef0fc48843b5
hangillee <skfcb10@naver.com>
2023-02-21 10:03:23 +0900

    chore: edit CORS config and OAuth2

--- src/main/java/gdsc/skhu/liferary/configure/WebConfig.java
+++ src/main/java/gdsc/skhu/liferary/configure/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "DELETE", "PATCH", "OPTIONS", "HEAD");
     }
